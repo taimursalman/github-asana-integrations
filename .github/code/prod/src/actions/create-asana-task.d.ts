ca2637c32164de9cf3ede49a9fef80ca3b8e6ab2
Ash <taimursalman@250mils.com>
2025-05-27 19:00:01 +0500

    pathing and output fixes

--- .github/code/prod/src/actions/create-asana-task.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const createAsanaTask: () => Promise<void>;
-//# sourceMappingURL=create-asana-task.d.ts.map
--- .github/code/prod/src/components/create-asana-task/create-asana-task-library.d.ts
+++
@@ -1,4 +0,0 @@
-import { AsanaUser } from './create-asana-task.types';
-export declare const findAsanaUserByEmail: (email: string, token: string, workspaceId: string) => Promise<AsanaUser | null>;
-export declare const getWorkspaceGid: (token: string) => Promise<string>;
-//# sourceMappingURL=create-asana-task-library.d.ts.map
